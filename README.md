39a3486c041ef569d4e1c1e14d3b09dd1fba004e
Dmytro Stoliaruk <dmytro.stoliaruk@gmail.com>
2023-10-12 18:02:06 +0300

    Add model Books, CRUD, tests

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # README
 
-1. Initial commit
+0. Initial commit
   - create project, database
   - added gems (rspec, devise)
   - modified database.yml (username, password - environment variables)
+
+1. Pull request
+  - create model 
+  - create controller 
+  - create views
+  - add validations for Books
+  - add tests
---
+++ spec/models/book_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  # create object for testing
+  subject { described_class.new(title: "Test Book", author: "Test Author", isbn: "123-12345-123-1234") }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without an author" do
+    subject.author = nil
+    expect(subject).to_not be_valid
+  end
+
+end 
+
