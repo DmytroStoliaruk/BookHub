729bce05471b5dbd457512455b3602fc63b4f6d9
Dmytro Stoliaruk <dmytro.stoliaruk@gmail.com>
2023-10-31 21:57:06 +0200

    update using FactoryBot

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :book do
-    title { Faker::Book.title }
+    sequence (:title) { |n| "№#{n} " + Faker::Book.title }
     author { Faker::Book.author }
     isbn { Faker::Code.unique.isbn }
     description { Faker::Lorem.paragraph }
+
+    trait :empty_title do
+      title { "" }
+    end
   end
 end
