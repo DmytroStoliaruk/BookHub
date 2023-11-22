4425c973c42f3e027dbcc9d52a60a321abee3cfb
Dmytro Stoliaruk <dmytro.stoliaruk@gmail.com>
2023-11-22 15:46:29 +0200

    update using search index

--- app/chewy/books_index.rb
+++ app/chewy/books_index.rb
@@ -1,7 +1,8 @@
 class BooksIndex < Chewy::Index
-  index_name { "books_#{Rails.env}" }
+  index_name("books_#{Rails.env}")
   index_scope Book
   field :title
   field :author
   field :description
+  reset
 end
