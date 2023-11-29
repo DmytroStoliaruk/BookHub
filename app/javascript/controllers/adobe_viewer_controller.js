81a043733b49323ce0a838ccc10923070d943fbe
Dmytro Stoliaruk <dmytro.stoliaruk@gmail.com>
2023-11-29 21:10:45 +0200

    add stimulus controller

--- app/javascript/controllers/adobe_viewer_controller.js
+++ app/javascript/controllers/adobe_viewer_controller.js
@@ -1,22 +1,25 @@
-import { Controller } from "stimulus";
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { url: String, filename: String }
 
   connect() { 
     this.initializeAdobeViewer();
   }
 
   initializeAdobeViewer() {
-    let adobeDCView = new AdobeDC.View({ clientId: "e748c44682a84ca29c02e975f648cd41", divId: this.element.id });
+    const url = this.data.get("url");
+    const filename = this.data.get("filename");
+    const divId = this.data.get("element-id");
+
+    let adobeDCView = new AdobeDC.View({ clientId: "e748c44682a84ca29c02e975f648cd41", divId: divId });
     adobeDCView.previewFile({
-      content: { location: { url: this.urlValue } },
-      metaData: { fileName: this.filenameValue }
+      content: { location: { url: url } },
+      metaData: { fileName: filename }
     },{
       embedMode: "IN_LINE",
       showAnnotationTools: false,
       showDownloadPDF: false,
       showPrintPDF: false
-    });
+    });    
   }
 }
